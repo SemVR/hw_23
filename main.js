9b3339151d24b25336e0ef86345ae08b2516652a
SemVR <sem.wlad26@gmail.com>
2025-05-08 15:38:42 +0300

    fix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //#buttonClick
 import handleButtonClick from './handleButtonClick.js'
 
+
 handleButtonClick('myButton', 'Button clicked!');
 
 //#trackMousePosition
@@ -11,13 +12,21 @@ console.log(trackMousePosition())
 //#setupEventDelegation
 import setupEventDelegation from './setupEventDelegation.js'
 function createTestList() {
-  document.body.innerHTML = `
-    <ul id="testList">
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-    </ul>
-  `;
+  let ul = document.createElement('ul');
+  ul.id = 'testList';
+  for (let i = 1; i <=3; i++){
+    let li = document.createElement('li');
+    li.textContent = `Text ${i}`;
+    ul.appendChild(li);
+  }
+  document.body.appendChild(ul);
+  
+  // document.body.innerHTML = 
+  //   `<ul id="testList">
+  //     <li>Item 1</li>
+  //     <li>Item 2</li>
+  //     <li>Item 3</li>
+  //   </ul>`
 }
 
 createTestList();
